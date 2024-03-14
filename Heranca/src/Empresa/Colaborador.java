cbe590fc711ad491823956cb42c627105ee6962b
ZeFurtado <lukhasfurtado9@gmail.com>
2024-03-14 18:03:12 -0300

    Criado construtor da classe Colaborador

--- Heranca/src/Empresa/Colaborador.java
+++ Heranca/src/Empresa/Colaborador.java
@@ -1,4 +1,20 @@
 package Empresa;
 
+import java.sql.Date;
+
 public class Colaborador {
+
+    private String nome;
+    private Date dataDeNascimento;
+    private float salario;
+    private Funcao funcao;
+    private Setor setor;
+
+    public Colaborador (String nome, Date dataDeNascimento, float salario, Funcao funcao, Setor setor){
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+        this.salario = salario;
+        this.funcao = funcao;
+        this.setor = setor;
+    }
 }
