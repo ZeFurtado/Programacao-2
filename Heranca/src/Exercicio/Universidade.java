e9d3b1b2cfe1b6f26a43b6d9ba2937b3c2f24941
ZeFurtado <lukhasfurtado9@gmail.com>
2024-03-16 18:57:50 -0300

    Criado os métodos de acesso get e set dos atributos da classe Universidade.java

--- Heranca/src/Exercicio/Universidade.java
+++ Heranca/src/Exercicio/Universidade.java
@@ -1,4 +1,40 @@
 package Exercicio;
 
 public class Universidade {
+    private String nome;
+    private String endereco;
+    private int qntdAlunos;
+    private int qntdProfessores;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setQntdAlunos(int qntdAlunos){
+        this.qntdAlunos = qntdAlunos;
+    }
+
+    public int getQntdAlunos() {
+        return qntdAlunos;
+    }
+
+    public void setQntdProfessores(int qntdProfessores) {
+        this.qntdProfessores = qntdProfessores;
+    }
+
+    public int getQntdProfessores() {
+        return qntdProfessores;
+    }
 }
