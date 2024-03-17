3a9a575dfd22d7197960bbb36d667ec3940ada90
ZeFurtado <lukhasfurtado9@gmail.com>
2024-03-17 10:15:10 -0300

    Criado os métodos da classe Privada

---
+++ Heranca/src/Exercicio/Privada.java
@@ -0,0 +1,35 @@
+package Exercicio;
+
+public class Privada extends Universidade {
+
+    private float mensalidade;
+
+    public Privada(float mensalidade){
+        this.mensalidade = mensalidade;
+    }
+
+    public void setMensalidade(float mensalidade) {
+        this.mensalidade = mensalidade;
+    }
+
+    public float getMensalidade() {
+        return mensalidade;
+    }
+
+    public void adicionaPreco(float preco){
+        mensalidade = mensalidade + preco;
+    }
+
+    @Override
+    public void print() {
+        StringBuilder resumo = new StringBuilder();
+        resumo.append("Nome da Universidade: "+getNome()+"\n"+
+                "Endereço: " + getEndereco()+"\n"+
+                "Quantidade de Alunos: " +getQntdAlunos()+ "\n"+
+                "Quantidade de Professores" +getQntdProfessores()+ "\n"+
+                "Valor da mensalidade: " +getMensalidade());
+
+        System.out.print(resumo.toString());
+    }
+    }
+}
