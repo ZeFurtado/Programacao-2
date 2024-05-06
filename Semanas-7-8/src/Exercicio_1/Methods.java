f2b832011fb947161136b63b01279cb5ecee2e78
ZeFurtado <lukhasfurtado9@gmail.com>
2024-05-05 23:17:07 -0300

    :sparkles: Métodos de pesquisa funcionando

--- Semanas-7-8/src/Exercicio_1/Methods.java
+++ Semanas-7-8/src/Exercicio_1/Methods.java
@@ -26,17 +26,44 @@ public class Methods {
     }
 
     public void pesquisaSequencial() throws IllegalArgumentException{
+        System.out.println("///////// Pesquisa Sequencial");
         if(!arrayEhOrdenado()) throw new IllegalArgumentException();
 
         int index = 0;
         for(int i : array){
-            if(i == 95){
+            if(i == target){
                 System.out.println(array[index] + " Achou!");
                 break;
             }
-            System.out.println(array[index] + " Não é 95");
+            System.out.println(array[index] + " Não é "+target);
             index++;
         }
+    }
+    public int pesquisaBinaria()throws IllegalArgumentException{
+        System.out.println("///////// Pesquisa Binária");
+        if(!arrayEhOrdenado()) throw new IllegalArgumentException();
+
+        int inf = 0;
+        int sup = array.length - 1;
+        int med;
+        int resultado = 0;
+
+        while (inf <= sup){
+            med = (inf + sup) / 2;
+
+            if(array[med] < target){
+                System.out.println(array[med]+ " Não é o " +target);
+                inf = med + 1;
+            }else if(array[med] > target){
+                System.out.println(array[med]+ " Não é o " +target);
+                sup = med - 1;
+            }else{
+                System.out.print("O número foi achado na posição " +med);
+                return array[med];
+            }
+        }
+
+        return -1;
     }
 
     private boolean arrayEhOrdenado(){
