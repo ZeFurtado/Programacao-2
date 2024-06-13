2583950136bd39d0f9f7638d86bf83aa89b3adb5
Lukhas Furtado <lukhasfurtado9@gmail.com>
2024-06-13 20:05:12 -0300

    Função contaNegativos() funcionando

--- Semanas-9-10/src/Exercicios.java
+++ Semanas-9-10/src/Exercicios.java
@@ -1,2 +1,21 @@
 public class Exercicios {
+    public int contaNegativos(StaticList<Integer> lista) throws IllegalArgumentException{
+        if(lista == null) throw new IllegalArgumentException();
+
+        return contaNegativos(lista, 0, 0);
+
+    }
+
+    private int contaNegativos(StaticList lista, int pos, int numNegativos){
+        if(pos == lista.numElements()) {
+            return numNegativos;
+        }else if((Integer)lista.get(pos) < 0){
+            numNegativos++;
+            pos++;
+            return contaNegativos(lista, pos, numNegativos);
+        }else {
+            pos++;
+            return contaNegativos(lista, pos, numNegativos);
+        }
+    }
 }
