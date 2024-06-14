12e69301177bb26b006ddb6a4795df23578f7579
Lukhas Furtado <lukhasfurtado9@gmail.com>
2024-06-14 19:32:09 -0300

    Criada função pública e privada exercicio4()

--- Semanas-9-10/src/Exercicios.java
+++ Semanas-9-10/src/Exercicios.java
@@ -41,4 +41,26 @@ public class Exercicios {
             }
         }
     }
+
+    public int exercicio4(StaticList<Integer> lista1, StaticList<Integer> lista2) throws IllegalArgumentException{
+        if(lista1 == null || lista2 == null) throw new IllegalArgumentException();
+        return exercicio4(lista1, lista2, 0, -1, -1);
+    }
+
+    private int exercicio4(StaticList<Integer> lista1, StaticList<Integer> lista2,int soma, int pos1, int pos2){
+        if((pos1 >= lista1.numElements() - 1) && (pos2 >= lista2.numElements() - 1)) return soma;
+
+        if(pos1 < lista1.numElements() - 1) {
+            soma += lista1.get(pos1 + 1);
+            pos1++;
+            return exercicio4(lista1, lista2, soma, pos1, pos2);
+        }else if(pos2 < lista2.numElements() - 1){
+            soma += lista2.get(pos2 + 1);
+            pos2++;
+            return exercicio4(lista1, lista2, soma, pos1, pos2);
+        }
+        
+        return - 1;
+    }
+
 }
