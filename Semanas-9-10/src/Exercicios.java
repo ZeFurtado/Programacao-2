f4f45e409fb8b92ff7f85a9a6e1b13af0b4a42a0
Lukhas Furtado <lukhasfurtado9@gmail.com>
2024-06-13 21:06:00 -0300

    Criada a função isPrimo()

--- Semanas-9-10/src/Exercicios.java
+++ Semanas-9-10/src/Exercicios.java
@@ -18,4 +18,12 @@ public class Exercicios {
             return contaNegativos(lista, pos, numNegativos);
         }
     }
+
+    public boolean isPrimo(int num){
+        if(num <= 0)return false;
+        for(int n = 2; n < num; n++){
+            if(num % n == 0) return false;
+        }
+        return true;
+    }
 }
