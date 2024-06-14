1c31b793db279676f13413fc2c04d8516892d714
Lukhas Furtado <lukhasfurtado9@gmail.com>
2024-06-14 19:52:54 -0300

    Criada a classe Eletrodomestico com seus atributos, construtor e métodos de acesso

---
+++ Semanas-9-10/src/Eletrodomestico.java
@@ -0,0 +1,37 @@
+public class Eletrodomestico {
+    private String marca;
+    private int voltagem;
+    private double valor;
+
+    public Eletrodomestico(String marca, int voltagem, double valor) {
+        this.marca = marca;
+        this.voltagem = voltagem;
+        this.valor = valor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(int voltagem) {
+        this.voltagem = voltagem;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+}
+
