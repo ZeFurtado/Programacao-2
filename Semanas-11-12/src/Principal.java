1964186646b612a6a156bfe5a567d926cb03879e
ZeFurtado <lukhasfurtado9@gmail.com>
2024-06-16 15:53:51 -0300

    Concluído Exercicio 2

--- Semanas-11-12/src/Principal.java
+++ Semanas-11-12/src/Principal.java
@@ -29,5 +29,37 @@ public class Principal {
         System.out.print("\n"+p2.toString());
         System.out.print("\n"+p3.toString());
          */
+
+        //Exercicio 2
+        /*
+        MinhaPilha<Integer> p1 = new MinhaPilha<Integer>(5);
+        MinhaPilha<Integer> p2 = new MinhaPilha<Integer>(5);
+        MinhaPilha<Integer> p3 = new MinhaPilha<Integer>(5);
+        try {
+            p1.push(10);
+            p1.push(20);
+            p1.push(30);
+            p1.push(40);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try{
+            p2.push(p1.pop());
+            p2.push(p1.pop());
+            p3.push(p1.pop());
+            p3.push(p1.pop());
+            p1.push(p2.pop());
+            p3.push(p2.pop());
+            p3.push(p1.pop());
+
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+         */
     }
 }
