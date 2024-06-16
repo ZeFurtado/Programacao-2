e95d49c6deda8ec374ea2ec338b53f04998ed8d4
ZeFurtado <lukhasfurtado9@gmail.com>
2024-06-16 15:07:43 -0300

    Exercicio 1 concluído

---
+++ Semanas-11-12/src/Principal.java
@@ -0,0 +1,33 @@
+public class Principal {
+    public static void main(String[] args) {
+        //Exercicio 1
+        /*
+        StaticStack<Integer> p1 = new StaticStack<Integer>(5);
+        StaticStack<Integer> p2 = new StaticStack<Integer>(5);
+        StaticStack<Integer> p3 = new StaticStack<Integer>(5);
+
+        p1.push(1);
+        p1.push(2);
+        p1.push(3);
+        p1.push(4);
+
+        try{
+            int temp = p1.pop();
+            p2.push(temp);
+            p3.push(p1.pop());
+            p2.push(p1.pop());
+            temp = p1.pop();
+            p3.push(temp);
+            p1.push(p2.pop());
+            p3.push(p2.pop());
+            p3.push(p1.pop());
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        System.out.print("\n"+p1.toString());
+        System.out.print("\n"+p2.toString());
+        System.out.print("\n"+p3.toString());
+         */
+    }
+}
